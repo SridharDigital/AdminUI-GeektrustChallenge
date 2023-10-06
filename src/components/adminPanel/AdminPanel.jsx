cd71fad9574239348ea909c4241274c261ef523e
Sreedharan Namboodiri C <89787284+SridharDigital@users.noreply.github.com>
2023-10-06 23:30:35 +0530

    Created UI fro admin dashboard, added delete functionality and update select status of the users.

--- src/components/adminPanel/AdminPanel.jsx
+++ src/components/adminPanel/AdminPanel.jsx
@@ -1,8 +1,32 @@
 import "./adminPanel.css"
 import { useState, useEffect } from "react"
 
+import UserItem from "../userItem/UserItem"
+
 const AdminPanel = () => {
   const [users, setUsers] = useState([])
+  const [currentPage, setCurrentPage] = useState(1)
+
+  const updateSelectStatus = (userId, selectStatus) => {
+    const updatedUsers = users.map((user) => {
+      if (userId === user.id) {
+        return {
+          ...user,
+          isSelected: event.target.checked,
+        }
+      } else {
+        return {
+          ...user,
+        }
+      }
+    })
+    setUsers(updatedUsers)
+  }
+
+  const deleteUser = (userId) => {
+    const updatedUsers = users.filter((user) => user.id !== userId)
+    setUsers(updatedUsers)
+  }
 
   useEffect(() => {
     ;(async () => {
@@ -10,16 +34,50 @@ const AdminPanel = () => {
         const fetchedData = await fetch(
           "https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json"
         )
-        const users = await fetchedData.json()
-        setUsers(users)
+        const fetchedUsers = await fetchedData.json()
+        const updatedUserData = fetchedUsers.map((user) => ({
+          ...user,
+          isSelected: false,
+        }))
+        setUsers(updatedUserData)
       } catch (error) {
         console.log(error)
       }
     })()
   }, [])
 
+  const currentPageUsers = users.slice(0, 9)
+
   console.log({ users })
-  return <div>AdminPanel</div>
+
+  return (
+    <div className="content-wrapper">
+      <input
+        type="search"
+        className="users-search-bar"
+        placeholder="Search by name, email or role"
+      />
+      <ul className="admin-dashboard-container">
+        <li className="user-item-container">
+          <div className="item-checkbox-container">
+            <input type="checkbox" className="item-checkbox" />
+          </div>
+          <p className="users-column-heading">Name</p>
+          <p className="users-column-heading">Email</p>
+          <p className="users-column-heading">Role</p>
+          <p className="users-column-heading">Actions</p>
+        </li>
+        {currentPageUsers.map((user) => (
+          <UserItem
+            user={user}
+            key={user.id}
+            updateSelectStatus={updateSelectStatus}
+            deleteUser={deleteUser}
+          />
+        ))}
+      </ul>
+    </div>
+  )
 }
 
 export default AdminPanel
--- src/components/userItem/UserItem.jsx
+++ src/components/userItem/UserItem.jsx
@@ -1,7 +1,34 @@
 import "./userItem.css"
+import trashIcon from "../../assets/trashIcon.svg"
+import editIcon from "../../assets/editIcon.svg"
 
-const UserItem = () => {
-  return <div>UserItem</div>
+const UserItem = ({ user, updateSelectStatus, deleteUser }) => {
+  const onClickCheckBox = (event) =>
+    updateSelectStatus(user.id, event.target.checked)
+
+  return (
+    <li className="user-item-container">
+      <div className="item-checkbox-container">
+        <input
+          type="checkbox"
+          className="item-checkbox"
+          onClick={onClickCheckBox}
+          checked={user.isSelected}
+        />
+      </div>
+      <p className="user-column-text">{user.name}</p>
+      <p className="user-column-text">{user.email}</p>
+      <p className="user-column-text">{user.role}</p>
+      <div className="user-item-action-container">
+        <img src={editIcon} className="trash-icon" />
+        <img
+          src={trashIcon}
+          className="trash-icon"
+          onClick={() => deleteUser(user.id)}
+        />
+      </div>
+    </li>
+  )
 }
 
 export default UserItem
